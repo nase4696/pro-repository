ccb4b400d0aa356ca32fa14bfa19ca8dd86896b1
有川瀬名 <arikawasena@arikawasenanoMacBook-Air.local>
2025-08-11 19:09:03 +0900

    新規登録e2eテスト実装

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -10,3 +10,33 @@ test("ログインに成功してホームページに遷移する", async ({ pa
   await expect(page).toHaveURL("http://localhost:3001/home");
   await expect(page.getByText("ログインしました")).toBeVisible();
 });
+
+test("新規登録に成功してホームページに遷移する", async ({ page }) => {
+  await page.goto("http://localhost:3001/register");
+
+  await page.getByLabel("ユーザーネーム").fill("新規ユーザー");
+  await page.getByLabel("メールアドレス").fill("signup@example.com");
+  await page.getByTestId("password-input").fill("password123");
+  await page.getByTestId("password-confirm-input").fill("password123");
+
+  await page.getByTestId("signup-button").click();
+
+  await expect(page).toHaveURL("http://localhost:3001/home");
+  await expect(page.getByText("新規登録が完了しました")).toBeVisible();
+  await expect(page.getByText("新規ユーザー")).toBeVisible();
+});
+
+test("未入力で新規登録ボタンをクリックするとエラーメッセージが表示される", async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3001/register");
+
+  await page.getByTestId("signup-button").click();
+
+  await expect(page.getByText("ユーザー名を入力して下さい")).toBeVisible();
+  await expect(page.getByText("メールアドレスを入力して下さい")).toBeVisible();
+  // await expect(page.getByText("パスワードを入力して下さい")).toBeVisible();
+  // await expect(page.getByText("パスワードを入力して下さい")).toBeVisible();
+
+  await expect(page).not.toHaveURL("http://localhost:3001/home");
+});
