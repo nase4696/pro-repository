f4d44f7467b77146b9e79967794a79a1c284b898
有川瀬名 <arikawasena@arikawasenanoMacBook-Air.local>
2025-08-14 17:58:11 +0900

    ワークフローのコード修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isTestEnv = process.env.NODE_ENV === "test";
+
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    // テスト環境では.env.testを読み込む
+    envPath: isTestEnv ? ".env.test" : ".env.local",
+  },
 };
 
 export default nextConfig;
